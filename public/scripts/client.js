436aedb7c5344ae50c54b7139b52df46bd715231
bbjarvs <brettbjarvis@gmail.com>
2021-09-23 21:13:15 +0000

    loop through and show old tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,44 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  // --- our code goes here ---
+  // Fake data taken from initial-tweets.json
+  const tweets = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = (tweets) => {
+    // loops through tweets
+    tweets.forEach(tweet => {
+
+      // calls createTweetElement for each tweet
+      const oldTweet = createTweetElement(tweet);
+
+      // takes return value and appends it to the tweets container
+      $('#old-tweets-container').append(oldTweet);
+    });
+  };
+
   const createTweetElement = (tweetData) => {
     const markup = `
     <section class="old-tweets-container">
@@ -33,27 +70,8 @@ $(document).ready(function() {
   </section>
     `;
     return markup;
-  }
-  
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
-  }
-  
-  const $tweet = createTweetElement(tweetData);
-  
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  
-  $('#old-tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-  
-  
+  };
+
+  renderTweets(tweets);
+
 });
