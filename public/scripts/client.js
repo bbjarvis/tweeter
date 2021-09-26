b0e1e9f94fa04a254c4e73de1e0b5c78f91e01a7
bbjarvs <brettbjarvis@gmail.com>
2021-09-26 00:07:16 +0000

    add loadTweets() to load /tweets file to page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 $(document).ready(function() {
-  // Fake data taken from initial-tweets.json
-  const tweets = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
 
   const renderTweets = (tweets) => {
     // loops through tweets
@@ -72,15 +47,23 @@ $(document).ready(function() {
     return markup;
   };
 
-  renderTweets(tweets);
-
   $("#new-tweet-form").submit(function (event) {
     const tweetText = $('#tweet-text').serialize()
     console.log(tweetText)
     event.preventDefault();
 
     $.ajax("/tweets", {method: 'POST', data: tweetText});
-    
+
   })
+
+  const loadTweets = () => {
+    $.ajax('/tweets', { method: 'GET'})
+    .then(function (tweets) {
+    renderTweets(tweets);
+    })
+
+  };
+
+  loadTweets();
 
 });
